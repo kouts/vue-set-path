88ff3478dfc8d63fcdea88df3c581fd98377a8b7
Kouts <koutsaftakis@gmail.com>
2021-05-09 22:07:31 +0300

    ci: added semantic-release changelog

--- release.config.js
+++ release.config.js
@@ -7,9 +7,22 @@ module.exports = {
     }
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['docs/CHANGELOG.md']
+      }
+    ]
   ]
 }
